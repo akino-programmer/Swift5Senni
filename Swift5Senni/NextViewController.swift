bcd62aaa70ff6dff983d62b93f287c1afce5d842
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-26 07:20:23 +0900

    10times move next view.

---
+++ Swift5Senni/NextViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NextViewController.swift
+//  Swift5Senni
+//
+//  Created by 野口晃 on 2020/06/25.
+//  Copyright © 2020 Akira Noguchi. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Swift5Senni/ViewController.swift
+++ Swift5Senni/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func plus(_ sender: Any) {
+        
+        count = count + 1
+        
+        Label.text = String(count)
+        
+        if count == 10{
+            
+            count = 0
+            //画面遷移をする
+            performSegue(withIdentifier: "next", sender: nil)
+            
+        }
+    }
+    
+    
+    
 }
 
