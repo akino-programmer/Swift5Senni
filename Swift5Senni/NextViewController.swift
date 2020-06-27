ccef571730b0a186ae05fcbf93e978bc5788fca7
akino-programmer <noguchi_a7@yahoo.co.jp>
2020-06-27 16:36:51 +0900

    use show moving.

--- Swift5Senni/NextViewController.swift
+++ Swift5Senni/NextViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 
 class NextViewController: UIViewController {
 
+    
+    @IBOutlet weak var Label2: UILabel!
+    
+    var count2 = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        Label2.text = String(count2)
+        
         // Do any additional setup after loading the view.
     }
     
 
-    @IBAction func back(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-        
-    }
+   
     
     
     
